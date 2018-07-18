0a3179c227904f9c0698b5f2b246919d85ea33af
Rafael José da Costa Gloria <rafael.gloria@cultura.gov.br>
2018-07-18 14:54:59 -0300

    [UPDATE]Planejamento Orçamentário-Atualizar execução orçamentária. Issue #510
    Inserindo comentário do url e finalidade das rotinas diarias.

--- seguranca/www/scripts_exec/spo_atualizarExecucaoOrcamentaria.php
+++ seguranca/www/scripts_exec/spo_atualizarExecucaoOrcamentaria.php
@@ -1,4 +1,24 @@
 <?php
+
+/**
+ * Carrega os dados financeiros do SIOP para a base do SIMEC.
+ *
+ * Assim que termina de baixar os dados financeiros, o script roda um processamento
+ * que coloca os dados na tabela <tt>spo.siopexecucao</tt>. O acompanhamento das páginas
+ * da execução já baixadas é feito na tabela <tt>spo.siopexecucao_acompanhamento</tt>.
+ * Ao final da execução, é enviado um e-mail com o resultado do processo.
+ *
+ * Sequência de execução:<br />
+ * <ol><li>Baixa os dados do webservice (WSQuantitativo.consultarExecucaoOrcamentaria);</li>
+ * <li>Apaga os dados da tabela wssof.ws_execucaoorcamentaria;</li>
+ * <li>Insere os dados retornados pelo webservice na tabela wssof.ws_execucaoorcamentaria;</li>
+ * <li>Executa o script de atualização de finaceiros na seguinte tabela: spo.siopexecucao;</li>
+ * <li>Envia e-mail com resultado da execução.</li></ol>
+ *
+ * @version $Id: spo_BaixarDadosFinanceirosSIOP.php 101880 2015-08-31 19:50:33Z maykelbraz $
+ * @link http://siminc2.cultura.gov.br/seguranca/scripts_exec/spo_BaixarDadosFinanceirosSIOP.php URL de execução.
+ */
+
 define('BASE_PATH_SIMEC', realpath(dirname(__FILE__) . '/../../../'));
 require_once BASE_PATH_SIMEC . "/global/config.inc";
 include_once APPRAIZ."planacomorc/classes/controller/ImportarDadosSiop.inc";
